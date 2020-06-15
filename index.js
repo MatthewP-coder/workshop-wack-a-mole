13d5964656e12fa99b58b80b419d25964c6a5aa7
unknown <matthewpiedra1228@gmail.com>
2020-06-15 18:03:30 -0400

    Progress so far

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 let score = 0;
+var scoreDisplay = document.getElementById('score');
+
+var holes = Array.from(document.getElementsByClassName("hole"));
+
+var randomHoleIndex = Math.floor((Math.random() * 9));
+setInterval(function() { holes[randomHoleIndex].classList.toggle("mole") }, 500);
