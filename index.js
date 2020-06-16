d5097cda385f19406d7e5cce9fda5c52cf28a596
unknown <matthewpiedra1228@gmail.com>
2020-06-16 10:39:13 -0400

    Final commit

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ var scoreDisplay = document.getElementById('score');
 
 var holes = Array.from(document.getElementsByClassName("hole"));
 
-var randomHoleIndex;
 setInterval(function() {
-    randomHoleIndex = Math.floor((Math.random() * holes.length));
+    var randomHoleIndex = Math.floor((Math.random() * holes.length));
     holes[randomHoleIndex].classList.toggle("mole");
-}, 300);
+}, 250);
 
 const gameArea = document.getElementById("whack-a-mole");
 gameArea.addEventListener('click', function (evt) {
    if(evt.target.matches(".mole")) {
        score++;
        scoreDisplay.innerText = score;
-       var click_index = holes.indexOf(evt.target)
+       var click_index = holes.indexOf(evt.target);
        holes[click_index].classList.toggle("mole",false);
+       // Or evt.target.classList.remove("mole");
    }
-})
+});
